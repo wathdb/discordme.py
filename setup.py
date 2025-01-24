a382101aeef41c547effcb0fe68b56e174d9d0c2
wathD <114563307+wathdb@users.noreply.github.com>
2025-01-24 16:54:31 +0100

    First add
    Adding setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discordme.py",
+    version="0.1.0",
+    author="wathD",
+    author_email="wathd_developement@proton.me",
+    description="Une bibliothèque Python open-source permetant d'interagir simplement avec Discord",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wathdb/discordme.py",
+    packages=find_packages(),
+    install_requires=[],  # Ajoutez des dépendances ici
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
