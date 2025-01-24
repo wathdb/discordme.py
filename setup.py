8a71baec1d4b094f5bd8afb062efb8477cee90c3
wathD <114563307+wathdb@users.noreply.github.com>
2025-01-24 18:51:18 +0100

    Update setup.py
    Updating version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="discordme.py",
-    version="0.1.0",
+    name="discordme",
+    version="0.2.0",
     author="wathD",
     author_email="wathd_developement@proton.me",
     description="Une bibliothèque Python open-source permetant d'interagir simplement avec Discord",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/wathdb/discordme.py",
     packages=find_packages(),
