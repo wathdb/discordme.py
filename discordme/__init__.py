626c562ff372791c5a8dea78e832c602d36fcc59
wathD <114563307+wathdb@users.noreply.github.com>
2025-01-25 17:17:43 +0100

    Update __init__.py
    Adding new functions

--- discordme/__init__.py
+++ discordme/__init__.py
@@ -1,7 +1,8 @@
 # discordme/__init__.py
 
-from .discord_api import get_token, add_reaction, send_mp, change_status, remove_status, change_bio, send_message
+from .discord_api import get_token, add_reaction, send_mp, change_status, remove_status, change_bio, send_message, reply_mp, get_channel_messages, create_channel, get_server_channels, get_all_server_messages, get_friends
 from .utils import encode_to_url_format
+
 
 __all__ = [
     'get_token', 
@@ -11,5 +12,11 @@ __all__ = [
     'remove_status',
     'change_bio',
     'send_message',
+    'reply_mp',
+    'get_channel_messages',
+    'create_channel',
+    'get_server_channels',
+    'get_all_server_messages',
+    'get_friends',
     'encode_to_url_format'
 ]
