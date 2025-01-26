7cd1eac099ae53d3c852046ca6b9aacedbfb6500
wathD <114563307+wathdb@users.noreply.github.com>
2025-01-26 16:35:00 +0100

    Update __init__.py
    New functions -> vocal functions

--- discordme/__init__.py
+++ discordme/__init__.py
@@ -1,6 +1,6 @@
 # discordme/__init__.py
 
-from .discord_api import get_token, add_reaction, send_mp, change_status, remove_status, change_bio, send_message, reply_mp, get_channel_messages, create_channel, get_server_channels, get_friends, block_user, unblock_user
+from .discord_api import get_token, add_reaction, send_mp, change_status, remove_status, change_bio, send_message, reply_mp, get_channel_messages, create_channel, get_server_channels, get_friends, block_user, unblock_user, typing, send_soundboard, join_vocal
 from .utils import encode_to_url_format
 
 
@@ -19,5 +19,8 @@ __all__ = [
     'get_friends',
     'block_user',
     'unblock_user',
+    'typing',
+    'send_soundboard',
+    'join_vocal',
     'encode_to_url_format'
 ]
